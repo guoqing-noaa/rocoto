a983f96561d755f204cde82a0d19b4fa34004b7e
guoqing.ge <Guoqing.Ge@noaa.gov>
2026-09-09 22:14:59 -0600

    always clear the in-progress submit flag, even when submit raises

--- lib/workflowmgr/bqs.rb
+++ lib/workflowmgr/bqs.rb
@@ -76,6 +76,7 @@ module WorkflowMgr
         # Submit the job
         @status[task.attributes[:name]][cycle.to_i]=@batchsystem.submit(task)
 
+      ensure
         # Mark this job submission as done
         @running[task.attributes[:name]][cycle.to_i] = false
 
